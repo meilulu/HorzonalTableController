9c101835c6bf16544ac2127c347d647447344916
ShareAnimation <ShareAnimation@shareanimation.local>
2016-05-30 14:05:54 +0800

    Initial Commit

---
+++ HorizonalController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HorizonalController
+//
+//  Created by ShareAnimation on 16/5/30.
+//  Copyright © 2016年 爱丽丝的梦境. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
